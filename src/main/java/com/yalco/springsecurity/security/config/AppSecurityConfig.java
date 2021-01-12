6fe76cc2cbb4d691762b1b96ff1b9119ca199a2e
Yalchan Niyaziev <yalcin_2009@abv.bg>
2021-01-12 16:31:13 +0200

    Add ldap authentication configuration with beans

--- src/main/java/com/yalco/springsecurity/security/config/AppSecurityConfig.java
+++ src/main/java/com/yalco/springsecurity/security/config/AppSecurityConfig.java
@@ -1,33 +1,41 @@
 package com.yalco.springsecurity.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.ldap.core.ContextSource;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.authentication.BindAuthenticator;
+import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
+import org.springframework.security.ldap.authentication.LdapAuthenticator;
 
 @Configuration
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().fullyAuthenticated()
-                .and()
-                .formLogin();
+        http.httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //for postman to not save jsession cookie
+        http.authorizeRequests().anyRequest().authenticated();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.ldapAuthentication()
-                .userDnPatterns("uid={0},ou=people")
-                .groupSearchBase("ou=groups")
-                .contextSource()
-                .url("ldap://localhost:8389/dc=springframework,dc=org")
-                .and()
-                .passwordCompare()
-                .passwordEncoder(new BCryptPasswordEncoder())
-                .passwordAttribute("userPassword");
+    @Bean
+    public ContextSource contextSource() {
+        return new DefaultSpringSecurityContextSource("ldap://localhost:8389/dc=example,dc=com");
+    }
+
+    @Bean
+    public LdapAuthenticator bindAuthenticator(BaseLdapPathContextSource contextSource) {
+        BindAuthenticator bindAuthenticator = new BindAuthenticator(contextSource);
+        bindAuthenticator.setUserDnPatterns(new String[]{"uid={0},ou=people"});
+        return bindAuthenticator;
+    }
+
+    @Bean
+    public LdapAuthenticationProvider authenticationProvider(LdapAuthenticator ldapAuthenticator){
+        return new LdapAuthenticationProvider(ldapAuthenticator);
     }
 }
