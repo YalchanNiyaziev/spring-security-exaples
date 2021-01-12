27a0301faf5ce9da4a5487ae944189ea8fd80482
Yalchan Niyaziev <yalcin_2009@abv.bg>
2021-01-12 17:09:13 +0200

    Add ldap authentication and authorization with bean config

--- src/main/java/com/yalco/springsecurity/api/HomeController.java
+++ src/main/java/com/yalco/springsecurity/api/HomeController.java
@@ -1,16 +1,27 @@
 package com.yalco.springsecurity.api;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    public String getHome(){
+    public String getHome() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        String username = authentication.getName();
+        Object principal = authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         return "Home page";
     }
     @PostMapping
--- src/main/java/com/yalco/springsecurity/security/config/AppSecurityConfig.java
+++ src/main/java/com/yalco/springsecurity/security/config/AppSecurityConfig.java
@@ -13,45 +13,50 @@ import org.springframework.security.ldap.authentication.BindAuthenticator;
 import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
 import org.springframework.security.ldap.authentication.LdapAuthenticator;
 import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
+import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 
 @Configuration
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.ldapAuthentication()
-                .contextSource()
-                    .url("ldap://localhost:8389/dc=example,dc=com")
-                .and()
-                .userSearchBase("ou=people")
-                .userSearchFilter("(uid={0})");
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //for postman to not save jsession cookie
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests()
+                .mvcMatchers("/api/admin").hasAnyRole("ADMIN")
+                .mvcMatchers("/api/reports").hasAnyRole("USER")
+                .anyRequest().permitAll();
     }
-//
-//    @Bean
-//    public ContextSource contextSource() {
-//        return new DefaultSpringSecurityContextSource("ldap://localhost:8389/dc=example,dc=com");
-//    }
-//
-//    @Bean
-//    public LdapAuthenticator bindAuthenticator(BaseLdapPathContextSource contextSource) {
-//        FilterBasedLdapUserSearch filterBasedLdapUserSearch =
-//                new FilterBasedLdapUserSearch("ou=people","(uid={0})",contextSource);
-//
-//        BindAuthenticator bindAuthenticator = new BindAuthenticator(contextSource);
-//        bindAuthenticator.setUserSearch(filterBasedLdapUserSearch);
-//        return bindAuthenticator;
-//    }
-//
-//    @Bean
-//    public LdapAuthenticationProvider authenticationProvider(LdapAuthenticator ldapAuthenticator){
-//        return new LdapAuthenticationProvider(ldapAuthenticator);
-//    }
+
+    @Bean
+    public ContextSource contextSource() {
+        return new DefaultSpringSecurityContextSource("ldap://localhost:8389/dc=example,dc=com");
+    }
+
+    @Bean
+    public LdapAuthenticator bindAuthenticator(BaseLdapPathContextSource contextSource) {
+        FilterBasedLdapUserSearch filterBasedLdapUserSearch =
+                new FilterBasedLdapUserSearch("ou=people","(uid={0})",contextSource);
+
+        BindAuthenticator bindAuthenticator = new BindAuthenticator(contextSource);
+        bindAuthenticator.setUserSearch(filterBasedLdapUserSearch);
+        return bindAuthenticator;
+    }
+
+    //Authorization
+    @Bean
+    public LdapAuthoritiesPopulator ldapAuthoritiesPopulator(BaseLdapPathContextSource contextSource){
+        String groupSearchBase = " ou=groups"; //authorization entry/base
+        DefaultLdapAuthoritiesPopulator authorities =
+                new DefaultLdapAuthoritiesPopulator(contextSource, groupSearchBase);
+        authorities.setGroupSearchFilter("uniqueMember={0}"); //authorization attribute
+        return authorities;
+    }
+
+    @Bean
+    public LdapAuthenticationProvider authenticationProvider(LdapAuthenticator ldapAuthenticator, LdapAuthoritiesPopulator ldapPopulator){
+        return new LdapAuthenticationProvider(ldapAuthenticator,ldapPopulator);
+    }
 }
